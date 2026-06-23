00c6970c372c898dda84fa627ce275da3dc68f50
creatorjun <100290157+creatorjun@users.noreply.github.com>
2026-06-23 10:52:14 +0900

    fix: switch to Type=simple, remove double-fork daemonize

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,7 @@
 // src/main.cpp
 #include "daemon/AuditDaemon.hpp"
 #include <syslog.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <cstdlib>
-
-static void daemonizeIfStandalone(ReceiverMode mode) {
-    if (mode == ReceiverMode::Dispatcher) {
-        return;
-    }
-
-    pid_t pid = fork();
-    if (pid < 0) { exit(EXIT_FAILURE); }
-    if (pid > 0) { exit(EXIT_SUCCESS); }
-    if (setsid() < 0) { exit(EXIT_FAILURE); }
-
-    pid = fork();
-    if (pid < 0) { exit(EXIT_FAILURE); }
-    if (pid > 0) { exit(EXIT_SUCCESS); }
-
-    umask(0027);
-    chdir("/");
-
-    int devNull = open("/dev/null", O_RDWR);
-    if (devNull >= 0) {
-        dup2(devNull, STDIN_FILENO);
-        dup2(devNull, STDOUT_FILENO);
-        dup2(devNull, STDERR_FILENO);
-        close(devNull);
-    }
-}
 
 int main(int argc, char* argv[]) {
     openlog("audit-daemon", LOG_PID | LOG_CONS, LOG_DAEMON);
@@ -40,8 +11,6 @@ int main(int argc, char* argv[]) {
 
     DaemonConfig cfg;
     AuditDaemon::loadConfig(configPath, cfg);
-
-    daemonizeIfStandalone(cfg.mode);
 
     AuditDaemon daemon(cfg);
     daemon.run();
