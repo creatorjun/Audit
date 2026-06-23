659e665c63a83c7c64130b68301a393cf39e9836
creatorjun <100290157+creatorjun@users.noreply.github.com>
2026-06-23 10:27:13 +0900

    refactor: decouple receiver/parser threads, fix stop signal propagation

--- src/receiver/NetlinkReceiver.hpp
+++ src/receiver/NetlinkReceiver.hpp
@@ -3,6 +3,10 @@
 #include <functional>
 #include <string>
 #include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
 
 enum class ReceiverMode {
     Standalone,
@@ -19,7 +23,7 @@ using RawEventCallback = std::function<void(const AuditRawEvent&)>;
 
 class NetlinkReceiver {
 public:
-    NetlinkReceiver(RawEventCallback cb, ReceiverMode mode);
+    NetlinkReceiver(RawEventCallback cb, ReceiverMode mode, size_t maxQueue = 16384);
     ~NetlinkReceiver();
 
     bool start();
@@ -28,15 +32,23 @@ public:
 private:
     void runStandalone();
     void runDispatcher();
+    void dispatchLoop();
 
     bool setupAuditRules();
     void cleanupAuditRules();
 
     bool parseLine(const std::string& line, AuditRawEvent& ev);
 
-    RawEventCallback    m_callback;
-    ReceiverMode        m_mode;
-    int                 m_auditFd;
-    int                 m_epollFd;
-    std::atomic<bool>   m_running;
+    RawEventCallback            m_callback;
+    ReceiverMode                m_mode;
+    int                         m_auditFd;
+    int                         m_epollFd;
+    std::atomic<bool>           m_running;
+    size_t                      m_maxQueue;
+
+    std::thread                 m_ioThread;
+    std::thread                 m_dispatchThread;
+    std::mutex                  m_queueMutex;
+    std::condition_variable     m_queueCv;
+    std::queue<AuditRawEvent>   m_rawQueue;
 };
